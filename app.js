6f34ddd828748fe89b6a5d7faa7e32dd47a9d045
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-01-02 20:10:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let arr = []
+const port = 8000;
+
+app.get("/",(req,res)=> {
+    res.send("HELLO WORLD:");
+});
+
+app.post("/",(req,res)=> {
+    const {name} = req.body;
+
+    arr.push(name);
+    res.send("HELLO WORLD:" + name);
+});
+
+app.listen(port, () => {
+    console.log("SERVER UP AT PORT: " + port);
+})
